545bc54f330d5eda9112a4345429609c3d506c83
Samuel Silva <samuelsljr@gmail.com>
2025-10-25 20:46:13 -0300

    35 push

--- script.js
+++ script.js
@@ -1,57 +1,40 @@
-function calcularqe(){
-    let vv = document.querySelector('#vv')
-    let nv = document.querySelector('#nv')
-    let valorvv = vv.value
-    let valornv = nv.value
-    let conta = (valorvv / valornv)
+function calcularQE(){
+    let vvE = parseInt(document.querySelector('#vv-e').value)
+    let numVP = parseInt(document.querySelector('#num-vp').value)
+    let conta = (vvE / numVP)
     let decimal = conta - Math.trunc(conta)
     let primeiroDecimal = Math.trunc(decimal * 10)
     if(primeiroDecimal <= 5){
-        document.getElementById('resultadoqe').innerHTML = Math.floor(valorvv/valornv)
-    } else{
-        document.getElementById('resultadoqe').innerHTML = Math.ceil(conta)
+        document.getElementById('resultado-qe').innerHTML = Math.floor(vvE/numVP)
+    }else{
+        document.getElementById('resultado-qe').innerHTML = Math.ceil(conta)
     }
 }
-function calcularqp(){
-    let vpc = document.querySelector('#vpc')
-    let qe = document.querySelector('#qe')
-    let valorvpc = vpc.value
-    let valorqe = qe.value
-    document.getElementById('resultadoqp').innerHTML = Math.floor(valorvpc / valorqe)
+function calcularQP(){
+    let vvPC = parseInt(document.querySelector('#vv-pc').value)
+    let qe = parseInt(document.querySelector('#qe').value)
+    document.getElementById('resultado-qp').innerHTML = Math.floor(vvPC / qe)
 }
-function calcular10qe(){
-    let dezqe = document.querySelector('#dezqe')
-    let valordezqe = dezqe.value
-    document.getElementById('resultado10qe').innerHTML = Math.floor(valordezqe * 0.1)
+function calcular10QE(){
+    let dezqe = parseInt(document.querySelector('#dezQE').value)
+    document.getElementById('resultado10QE').innerHTML = Math.floor(dezqe * 0.1)
 }
-function calcular80qe(){
-    let oitentaqe = document.querySelector('#oitentaqe')
-    let valoroitentaqe = oitentaqe.value
-    document.getElementById('resultado80qe').innerHTML = Math.floor(valoroitentaqe * 0.8)
+function calcular80QE(){
+    let oitentaqe = parseInt(document.querySelector('#oitentaQE').value)
+    document.getElementById('resultado80QE').innerHTML = Math.floor(oitentaqe*0.8)
 }
-function calcularms1(){
-    let msvpc = document.querySelector('#msvpc')
-    let msqp = document.querySelector('#msqp')
-    let valormsvpc = msvpc.value
-    let valormsqp = parseInt(msqp.value)
-    document.getElementById('resultadomsum').innerHTML = Math.floor(valormsvpc / (valormsqp + 1))
+function calcularMS1(){
+    let msVvPc = parseInt(document.querySelector('#ms-vv-pc').value)
+    let msQpPc = parseInt(document.querySelector('#ms-qp-pc').value)
+    document.getElementById('resultado-ms-um').innerHTML = Math.floor(msVvPc/(msQpPc+1))
 }
-function calcular20qe(){
-    let vinteqe = document.querySelector('#vinteqe')
-    let valorvinteqe = vinteqe.value
-    document.getElementById('resultado20qe').innerHTML = Math.floor(valorvinteqe * 0.2)
+function calcular20QE(){
+    let vinteqe = parseInt(document.querySelector('#vinteQE').value)
+    document.getElementById('resultado20QE').innerHTML = Math.floor(vinteqe * 0.2)
 }
-function calcularms2(){
-    let msvpcdois = document.querySelector('#msvpcdois')
-    let msqpdois = document.querySelector('#msqpdois')
-    let valormsvpcdois = msvpcdois.value
-    let valormsqpdois = parseInt(msqpdois.value)
-    document.getElementById('resultadomsdois').innerHTML = Math.floor(valormsvpcdois / (valormsqpdois + 1 + 1))
-}
-function calcularms3(){
-    let msvpctres = document.querySelector('#msvpctres')
-    let msqptres = document.querySelector('#msqptres')
-    let valormsvpctres = msvpctres.value
-    let valormsqptres = parseInt(msqptres.value)
-    document.getElementById('resultadomstres').innerHTML = Math.floor(valormsvpctres / (valormsqptres + 1 + 2))
+function calcularMSX() {
+    let vvPC = parseInt(document.querySelector('#ms-vv-pc-x').value)
+    let qpPC = parseInt(document.querySelector('#ms-qp-pc-x').value)
+    let vsPC = parseInt(document.querySelector('#ms-vs-pc-x').value)
+    document.getElementById('resultadomsx').innerHTML = Math.floor(vvPC / (qpPC + 1 + vsPC))
 }
